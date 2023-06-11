775409e7e82e092978693758cddd200bed14b651
Spandan Barve <spandan567@gmail.com>
2023-06-11 13:01:38 +0530

    fix

--- server/index.ts
+++ server/index.ts
@@ -14,15 +14,7 @@ app.use(cors({ origin: frontendUrl }));
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.use("/", indexRouter);
-
-app.get("/", (req, res) => {
-  res.send("are baba thikk hai");
-});
-
-app.get("/api", (req, res) => {
-  res.send("asia kam karta hai");
-});
+app.use("/api", indexRouter);
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
